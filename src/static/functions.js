9e1e9770233b0091e71955c3d590a60d72c5f6b7
Charlie He <charlie@Charlies-MacBook-Pro.local>
2012-10-16 19:50:13 -0400

    devnotes and button design

--- src/static/functions.js
+++ src/static/functions.js
@@ -1,6 +1,6 @@
 
 function activate() {
-	$("#activate").css("background","-webkit-gradient(linear, left top, left bottom, from(#BEFFBE), to(#69FF69))");
+	$("#activate").css("background","-webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#69FF69))");
 	$("#stop").css("background","-webkit-gradient(linear, left top, left bottom, from(#FF3F3F), to(#FF5959))");
 	
 	var str = JSON.parse(localStorage["Facebook-Lock"]);
@@ -21,7 +21,7 @@ function stop() {
 	if(str["activated"]=="true") {
 	
 		$("#activate").css("background","-webkit-gradient(linear, left top, left bottom, from(#4BE94B), to(#48C548))");
-		$("#stop").css("background","-webkit-gradient(linear, left top, left bottom, from(#FFB5B5), to(#FF9494))");
+		$("#stop").css("background","-webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#FF9494))");
 	
 		str["activated"]="false";
 		localStorage["Facebook-Lock"] = JSON.stringify(str);
@@ -49,7 +49,7 @@ function initButtons() {
 		str = JSON.parse(str);
 		if(!str["activated"]) {str["activated"]="false";localStorage["Facebook-Lock"] = JSON.stringify(str);}
 		if(str["activated"]=="true") {
-			$("#activate").css("background","-webkit-gradient(linear, left top, left bottom, from(#BEFFBE), to(#69FF69))");
+			$("#activate").css("background","-webkit-gradient(linear, left top, left bottom, from(#C9C9C9), to(#69FF69))");
 			$("#stop").css("background","-webkit-gradient(linear, left top, left bottom, from(#FF3F3F), to(#FF5959))");
 			chrome.browserAction.setIcon({path:"static/lock.png"});
 		}
